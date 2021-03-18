310eddb61335ae2194236201535b07aa94a5e527
luka <abuladzeluka12@Gmail.com>
2021-03-18 12:30:09 +0400

    EU ML

---
+++ first.py
@@ -0,0 +1,17 @@
+#print(4-(2**3)+5*2-3/2)
+
+#x=int(input())
+#y=int(input())
+#e=int(input())
+#ar=((x+y+e)/3)
+#print(ar)
+
+#x=int(input())
+#if x % 10 == 0:
+#    print("10-ს ჯერადია")
+#else:
+#    print("არ არის")
+
+
+
+
