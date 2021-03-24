5e3b56c90622ddf08ef5c4918377544c6323ae43
luka <abuladzeluka12@Gmail.com>
2021-03-24 20:02:27 +0400

    EU ML

---
+++ sec.py
@@ -0,0 +1,116 @@
+# a=int(input("თქვენი სამუშაო საათების რაოდენობა?"))
+# b=int(input("თქვენი სამუშაო საათობრივი ხელფასაი?"))
+# print(a*b)
+
+# a=input("რა გქვიათ?")
+# b=int(input("რამდენი წლის ბრძანდებით?"))
+# print(f"თქვენ გახდებით {a}", 100-b,"წელში,ასი წლის")
+
+# a=int(input("შეიყვანეთ რიცხვი"))
+# if a > 0:
+# 	print("Number is positive")
+# else:
+# 	print("უარყოფითია")
+
+# a=int(input("შეიყვანე რიცხვი"))
+# b=int(input("შეიყვანე რიცხვი"))
+# if a+b > 0:
+# 	print((a+b)//2)
+# else:
+# 	print(a*b)
+
+
+# number = int(input("შეიყვანე რიცხვი"))
+# last= number % 10
+# print(last)
+
+# year = int(input("შეიყვანე წელი: "))
+
+# if (year % 4) == 0:
+#    if (year % 100) == 0:
+#        if (year % 400) == 0:
+#            print(f"{year} ნაკიანია")
+#        else:
+#            print(f"{year} არ არის ნაკიანი")
+#    else:
+#        print(f"{year} ნაკიანია")
+# else:
+#     print(f"{year}არ არის ნაკიანი")
+
+
+# for x in range(20,125):
+# 	if x % 5 == 0:
+# 		print(x)
+
+# for x in range(200,25,-1):
+# 	if x % 8 == 0:
+# 		print(x)
+
+# num1 = int(input("შეიყვანე რიცხვი:"))
+# num2 = int(input("შეიყვანე რიცხვი: "))
+# div = 0
+# for i in range(1,min(num1,num2)+1):
+#   if num1%i == num2%i == 0:
+#     divisor = i
+#     print(div)
+
+
+
+# lst = []
+# n = int(input("შეიყვანე რიცხვი : "))
+# for i in range(0, 2):
+# 	ele = int(input())
+# 	lst.append(ele)
+	
+# print(sum(lst)/len(lst))
+
+
+# for i in range(0,100):
+# 	if i % 2 == 0:
+# 		print(i)
+
+
+# for i in range(1500,2100):
+# 	if i % 7 == 0 and i % 5 == 0:
+# 		print(i)
+
+
+# lst = []
+# for i in range(1500,2100):
+# 	if i % 7 == 0 and i % 5 == 0:
+# 		lst.append(i)
+
+# print(sum(lst))
+
+
+# lst = []
+# for i in range(1500,2100):
+# 	if i % 7 == 0 and i % 5 == 0:
+# 		lst.append(i)
+# 		if sum(lst) < 20000:
+# 				break
+# print(sum(lst))
+
+
+# lst = []
+# for i in range(1500,2100):
+# 	if i % 7 == 0 and i % 5 == 0:
+# 		lst.append(i)
+
+# print(len(lst))
+
+# import math
+# a = int(input("შეიყვანე რიცხვი: "))
+# b= int(input("შეიყვანე რიცხვი: "))
+# print (math.gcd(a,b))
+
+# import math
+# a = int(input("შეიყვანე რიცხვი"))
+# b = int(input("შეიყვანე რიცხვი"))
+# print((a / math.gcd(a,b))* b)
+
+
+
+
+
+
